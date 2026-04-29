4fd1e4e9a0fa66672ca6b08abf0c4c883f005900
Rafael Reis <rafael.dnreis@gmail.com>
2026-04-29 15:01:05 -0300

    fix(start.sh): removed cd to /var/www, useful for docker, but breaks railway with 502 error on the online page

--- start.sh
+++ start.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-cd /var/www
-
 echo "Installing PHP dependencies..."
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
@@ -18,11 +16,6 @@ php artisan key:generate --force
 
 echo "Running migrations..."
 php artisan migrate --force
-
-echo "Caching config..."
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 
 echo "Starting server..."
 php artisan serve --host=0.0.0.0 --port=${PORT}
