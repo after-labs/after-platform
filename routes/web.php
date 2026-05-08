55cbe6642bc91c6c14c14d1e2de64cf091851e31
Rafael Reis <rafael.dnreis@gmail.com>
2026-05-08 17:51:35 -0300

    fix: separate public and admin game routes

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,19 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('frontend.home');
-});
+})->name('home');
 
-Route::get('/games', [GameController::class, 'index']);
+Route::get('/games', [GameController::class, 'index'])->name('games.index');
 
 Route::get('/games/show/{game}', [
     GameController::class,
     'show'
-]);
+])->name('games.show');
 
 Route::get('/games/category/{category}', [
     GameController::class,
     'category'
-]);
+])->name('games.category');
 
 Route::get('lang/{locale}', function ($locale) {
 
@@ -36,7 +36,7 @@ Route::get('lang/{locale}', function ($locale) {
 
     return redirect()->back();
 
-});
+})->name('lang.switch');
 
 
 
@@ -44,129 +44,125 @@ Route::get('lang/{locale}', function ($locale) {
 
 Route::middleware(['auth'])->group(function(){
 
-    Route::get('/account', function () {
-        return view('frontend.account.account');
-    });
+    Route::redirect('/account', '/profile')->name('account');
 
     // Cart
 
     Route::get('/cart', [
         CartItemController::class,
         'index'
-    ]);
+    ])->name('cart.index');
 
     Route::post('/cart/store/{gameVersion}', [
         CartItemController::class,
         'store'
-    ]);
+    ])->name('cart.store');
 
     Route::get('/cart/delete/{cartItem}', [
         CartItemController::class,
         'delete'
-    ]);
+    ])->name('cart.delete');
 
     // Orders
 
     Route::get('/orders', [
         OrderController::class,
         'index'
-    ]);
+    ])->name('orders.index');
 
     Route::get('/orders/checkout', [
         OrderController::class,
         'checkout'
-    ]);
+    ])->name('orders.checkout');
 
     // Profile
 
     Route::get('/profile', [
         ProfileController::class,
         'edit'
-    ]);
+    ])->name('profile.edit');
 
     Route::patch('/profile', [
         ProfileController::class,
         'update'
-    ]);
+    ])->name('profile.update');
 
     Route::delete('/profile', [
         ProfileController::class,
         'destroy'
-    ]);
+    ])->name('profile.destroy');
 
 });
 
 // Admin
 
-Route::middleware(['auth', 'admin'])->group(function(){
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function(){
 
-    Route::get('/admin', function () {
-        return view('backend.dashboard');
-    });
+    Route::redirect('/', '/admin/games')->name('dashboard');
 
     // Users
 
     Route::get('/users', [
         UserController::class,
         'index'
-    ]);
+    ])->name('users.index');
 
     Route::get('/users/create', [
         UserController::class,
         'create'
-    ]);
+    ])->name('users.create');
 
     Route::post('/users/store', [
         UserController::class,
         'store'
-    ]);
+    ])->name('users.store');
 
     Route::get('/users/edit/{user}', [
         UserController::class,
         'edit'
-    ]);
+    ])->name('users.edit');
 
     Route::post('/users/update/{user}', [
         UserController::class,
         'update'
-    ]);
+    ])->name('users.update');
 
     Route::get('/users/delete/{user}', [
         UserController::class,
         'delete'
-    ]);
+    ])->name('users.delete');
 
     // Games
 
     Route::get('/games', [
         GameController::class,
         'adminIndex'
-    ]);
+    ])->name('games.index');
 
     Route::get('/games/create', [
         GameController::class,
         'create'
-    ]);
+    ])->name('games.create');
 
     Route::post('/games/store', [
         GameController::class,
         'store'
-    ]);
+    ])->name('games.store');
 
     Route::get('/games/edit/{game}', [
         GameController::class,
         'edit'
-    ]);
+    ])->name('games.edit');
 
     Route::post('/games/update/{game}', [
         GameController::class,
         'update'
-    ]);
+    ])->name('games.update');
 
     Route::get('/games/delete/{game}', [
         GameController::class,
         'delete'
-    ]);
+    ])->name('games.delete');
 
 });
 
