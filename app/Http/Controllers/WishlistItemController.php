d272f78b2fb42f3c5a27c86fdac866c5bdfffd02
Rafael Reis <rafael.dnreis@gmail.com>
2026-05-09 09:58:46 -0300

    feat: add wishlist actions

--- app/Http/Controllers/WishlistItemController.php
+++ app/Http/Controllers/WishlistItemController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Game;
+use App\Models\WishlistItem;
+use Illuminate\Support\Facades\Auth;
 
 class WishlistItemController extends Controller
 {
-    //
+    public function index()
+    {
+        $items = WishlistItem::with([
+            'Game.media',
+            'Game.versions.offer',
+            'Game.category',
+        ])
+            ->where('user_id', Auth::id())
+            ->latest()
+            ->get();
+
+        return view('frontend.account.wishlist', compact('items'));
+    }
+
+    public function store(Game $game)
+    {
+        WishlistItem::firstOrCreate([
+            'user_id' => Auth::id(),
+            'game_id' => $game->id,
+        ]);
+
+        return redirect()->route('wishlist.index');
+    }
+
+    public function delete(WishlistItem $wishlistItem)
+    {
+        abort_unless($wishlistItem->user_id === Auth::id(), 403);
+
+        $wishlistItem->delete();
+
+        return redirect()->route('wishlist.index');
+    }
 }
